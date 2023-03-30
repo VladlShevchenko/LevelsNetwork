7c2f2da81888ae044eba7dd025fdcd82a4cd68ff
Ivan <denatarelcom2105@gmail.com>
2023-03-30 23:17:32 +0300

    setup finished

--- LevelsNetwork/config/urls.py
+++ LevelsNetwork/config/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""LevelsNetwork URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,12 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from djangoProject.views import article, article_year
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('events/2003/', article),
-    # path('events/<int:year>/', article_year),
-    re_path(r'^events/(?P<year>[0-9]{2}/$)', article_year)
 ]
