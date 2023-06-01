7abe028f176bbcae16085908b59de63d9b8da17e
Ivan <denatarelcom2105@gmail.com>
2023-06-02 00:04:43 +0300

    added static root var in settings.py

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+
+ROOT_PATH = os.path.dirname(__file__)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
